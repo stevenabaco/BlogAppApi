29b49f36b048b4c14c0523dc2236bd1b8d5e9d85
Steven Abaco <stevenabaco@gmail.com>
2020-09-30 11:07:13 -0500

    Create readData function to get data from data.json

--- api/models/posts.js
+++ api/models/posts.js
@@ -1,13 +1,30 @@
+const PATH = "./data.json"
+const fs = require('fs');
 class Post {
-    get() {
-        // ** Get Posts **
-
-        getIndividualBlog() {
-
-        }
-
-        add() {
-
-        }
+    
+    readData() {
+        let rawdata = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawdata);
+        return rawdata
     }
+
+    get() {
+        // ** Get Posts ** //
+        return this.readData();
+    }
+
+    getIndividualBlog() {
+        // ** Get individual blog post ** //
+
+    }
+
+    add() {
+        // ** Add new Post ** //
+
+    }
+
+    
 }
+
+
+module.exports = Post;
