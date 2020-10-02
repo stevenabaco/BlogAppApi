41ab2d51c8d56ceeee1e8213b425a82a1596fd44
Steven Abaco <stevenabaco@gmail.com>
2020-10-01 19:01:17 -0500

    Add ability to upload file to API with multer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const Post = require("./api/models/posts");
 const postsData = new Post();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     next();
@@ -17,11 +19,22 @@ app.get("/api/posts", (req, res) => {
 app.get("/api/posts/:post_id", (req, res) => {
     const postId = req.params.post_id;
     const foundPost = postsData.getIndividualBlog(postId);
-    if(foundPost) {
+    if (foundPost) {
         res.status(200).send(foundPost)
     } else {
         res.status(404).send("Not Found")
     }
 })
 
+app.post("/api/posts", (req, res) => {
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "post_image": req.body["post-image"],
+        "added_date": `${Date.now()}`
+    }
+    postsData.add(newPost);
+    res.status(201).send(newPost);
+})
 app.listen(3000, () => console.log("Listening on localhost:3000"));
