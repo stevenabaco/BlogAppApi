78e7c08ea168c733cea409a0596849c2cd184530
Steven Abaco <stevenabaco@gmail.com>
2020-10-01 19:01:50 -0500

    Finish adding ability to upload file to API

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 const Post = require("./api/models/posts");
+var multer = require("multer");
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`)
+    }
+})
+
+var upload = multer({
+    storage: storage
+});
+
+const getExt = (mimeType) => {
+    switch (mimeType) {
+        case "image/png":
+            return ".png";
+        case "image/jpg":
+            return ".jpg";
+        case "image/jpeg":
+            return ".jpeg";
+    }
+}
 const postsData = new Post();
 
 app.use(express.json());
@@ -26,15 +50,15 @@ app.get("/api/posts/:post_id", (req, res) => {
     }
 })
 
-app.post("/api/posts", (req, res) => {
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
     const newPost = {
         "id": `${Date.now()}`,
         "title": req.body.title,
         "content": req.body.content,
-        "post_image": req.body["post-image"],
+        "post_image": req.file.path,
         "added_date": `${Date.now()}`
     }
     postsData.add(newPost);
-    res.status(201).send(newPost);
+    res.status(201).send("ok");
 })
 app.listen(3000, () => console.log("Listening on localhost:3000"));
